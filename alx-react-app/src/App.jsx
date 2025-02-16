8a68128f6827a4d79bdc9e97b36749d3ccc07c8d
Abraham-trigs <abrahamtrigs@gmail.com>
2025-02-16 23:22:09 +0000

    WelcomeMessage added to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './src/components/WelcomeMessage' ''
+import WelcomeMessage from './src/components/WelcomeMessage' 
+
+function greet() {
+  return (
+      <div>
+          <WelcomeMessage />
+      </div>
+  );
+}
+
+export default greet;
 
 function App() {
   const [count, setCount] = useState(0)
