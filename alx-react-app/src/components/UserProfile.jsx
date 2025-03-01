377f5ad0629232d262508e5dfab8f7e511dfc7c2
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-01 11:04:39 +0000

    all components added to App.jsx and sucefully displaying

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
-function UserProfile = ({name, age, bio}) => {
+function UserProfile({ name, age, bio})  {
   return (
     <div>
-    <h2>{name}</h2>
+    <h2>Name: {name}</h2>
     <p>Age: {age}</p>
-    <p>{bio}</p>
+    <p>Bio: {bio}</p>
     </div>
-  )
+  );
 };
 
 export default UserProfile;
