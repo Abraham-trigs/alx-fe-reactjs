59ab058846c5b8319b8bad9d8be2b45b10778d54
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-01 10:52:59 +0000

    UserProfile.jsx  file created in ./src/components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
-// import Header from './src/component/Header';
-// import MainContent from './src/componens/MainContent';
-// import Footer from './src/components/Footer';
-// import UserProfile from './components/UserProfile';
-
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
