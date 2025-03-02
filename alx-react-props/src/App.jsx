d8162de654a67291c822b9cf9a39d4cd0b2f31c4
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-02 11:57:55 +0000

    all components created including UserContext.js

--- alx-react-props/src/App.jsx
+++ alx-react-props/src/App.jsx
@@ -1,26 +1,9 @@
-import { useState } from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-
+import ProfilePage from './ProfilePage';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-
-      <UserProfile
-      name= "Alice" 
-      age= "25" 
-      bio= "Loves hiking and photography" />
-    </>
-  )
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
