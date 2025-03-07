72a5366ce8af131bb0422dd7566530bc15a0480a
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-07 09:49:22 +0000

    recipeStore.js updated

---
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  setRecipes: (recipes) => set({ recipes }),
+
+  setRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+}));
+
+export default useRecipeStore;
