26c595fc4590c8e6f2f8e802a8ee26ee740373c9
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-07 16:20:47 +0000

    EditRecipeForm .jsx updated

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,16 +1,18 @@
 import { useRecipeStore } from './recipeStore';
 
-const RecipeDetails = ({ recipeId }) => {
-  const recipe = useRecipeStore(state =>
-    state.recipes.find(recipe => recipe.id === recipeId)
-  );
+  const RecipeDetails = ({ recipeId }) => {
+    const recipe = useRecipeStore(state =>
+      state.recipes.find(recipe => recipe.id === recipeId)
+    );
 
-  return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.description}</p>
-      {/* Render EditRecipeForm and DeleteRecipeButton here */}
-    </div>
-  );
-};
-export default RecipeDetails;
+    return (
+      <div>
+        <h1>{recipe.title}</h1>
+        <p>{recipe.description}</p>
+
+        <EditRecipeForm recipe={recipe} />  
+        <DeleteRecipeButton recipeId={recipeId} />
+      </div>
+    );
+  }; 
+  export default RecipeDetails;
