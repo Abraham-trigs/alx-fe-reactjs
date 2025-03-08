380d183e95558a4a6eebe10f831a3dcef609d204
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-08 12:56:40 +0000

    App.jsx update for

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,20 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+
 const App = () => {
-  
   return (
-    <>
-    <RecipeList />
-    <AddRecipeForm />
-
-    <Routes>
-      <Route path="/" element={<RecipeList />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-    </Routes>
-    </>
-  )
-
+    <Router> 
+      <Routes>
+        <Route path="/" element={
+          <> 
+            <RecipeList />
+            <AddRecipeForm />
+          </>
+        } />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
