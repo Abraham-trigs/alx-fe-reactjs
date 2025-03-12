986fa6d9605a5a14836038bc6d1f5899b19cf362
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-12 01:02:15 +0000

    BlogPost update to use id for Blog posting insteadofSlug

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const BlogPost = () => {
-  const { slug } = useParams(); // ✅ Get the slug from the URL
+  const { id } = useParams(); // ✅ Get the ID from the URL
 
   return (
     <div>
-      <h2>Blog Post: {slug}</h2>
-      <p>This is the content for the blog post with the slug: "{slug}".</p>
+      <h2>Blog Post {id}</h2>
+      <p>This is the content for blog post with ID: {id}.</p>
     </div>
   );
 };
