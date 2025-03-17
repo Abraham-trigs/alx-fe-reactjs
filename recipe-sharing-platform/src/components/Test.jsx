bc3eef3b774d8dae8959819736f2d4272b94118f
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-17 21:34:53 +0000

    tailwind configured and tested

---
+++ recipe-sharing-platform/src/components/Test.jsx
@@ -0,0 +1,17 @@
+
+const Test = () => {
+  return (
+    <h1 className="
+    text-blue-800
+    flex
+    flex-col
+    items-center
+    font-bold
+    
+    
+
+    ">WORIKING PERFECTLY</h1>
+  )
+};
+
+export default Test;
