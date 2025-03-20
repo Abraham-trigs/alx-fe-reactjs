6bde20344f2933893453fa5ea00de27f9a2b5b39
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-20 16:58:20 +0000

    grid-cols-1 added to Homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -38,7 +38,7 @@ function Homepage() {
       </div>
 
       {/* Display Recipes Grid */}
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {[...data, ...recipes].map((recipe, index) => (
           <div key={index} className="bg-white shadow-lg rounded-lg p-4 max-w-xs">
             {/* Recipe Image */}
