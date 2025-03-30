65f39342dad0f71fa7d65882b5a16a6c3d81d97b
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-30 15:45:01 +0000

    displaying a basic setup ready for further development

---
+++ github-user-search/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search users..."   
+      className="search-input"
+    />
+  );
+}
+  
+export default Home;
